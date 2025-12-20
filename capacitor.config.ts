6b9219e98a693961d8c32e5e1b3809cbbe3437cf
moh-d-m4x <moh.d.m4x@gmail.com>
2025-12-20 05:59:45 +0300

    chore: add capacitor.config.ts to gitignore

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-    appId: 'com.arcai.app',
-    appName: 'ARCAI',
-    webDir: 'dist'
-};
-
-export default config;
